d68669866e52b63b8a0d0ae279ba1d34e064f3f5
Lee, Dongkyu <ardor12@gmail.com>
2019-10-20 01:56:45 +0900

    [update] move from remote theme into local one

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Test",
+        "excerpt":"I need some tests to use github pages &amp; minimal mistakes theme functionalities.   To-do list     profile photo   categorization            menu bar           design            remote theme -&gt; local theme       font           ","categories": ["test"],
+        "tags": [],
+        "url": "http://localhost:4000/test/first-post/",
+        "teaser":null}]
